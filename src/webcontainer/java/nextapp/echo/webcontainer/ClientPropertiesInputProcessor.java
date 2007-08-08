6f6f40f0cc1c8d580b4b55f5e7c3f140a89e949e
tliebeck <tliebeck@a486e859-b728-0410-9144-a388b9637126>
2007-08-08 00:52:17 +0000

    ClientProperties working (albeit requires hitting reload first due to other soon-to-be-fixed bug).

--- src/webcontainer/java/nextapp/echo/webcontainer/ClientPropertiesInputProcessor.java
+++ src/webcontainer/java/nextapp/echo/webcontainer/ClientPropertiesInputProcessor.java
@@ -1,17 +1,53 @@
 package nextapp.echo.webcontainer;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.w3c.dom.Element;
 
+import nextapp.echo.app.serial.PropertyPeerFactory;
+import nextapp.echo.app.serial.SerialException;
+import nextapp.echo.app.serial.SerialPropertyPeer;
 import nextapp.echo.app.util.Context;
 import nextapp.echo.app.util.DomUtil;
 
 public class ClientPropertiesInputProcessor
 implements ClientMessage.Processor {
 
+    private static final Map TYPE_MAP;
+    static {
+        Map m = new HashMap();
+        m.put(ClientProperties.SCREEN_WIDTH, Integer.class);
+        m.put(ClientProperties.SCREEN_HEIGHT, Integer.class);
+        m.put(ClientProperties.SCREEN_COLOR_DEPTH, Integer.class);
+        m.put(ClientProperties.UTC_OFFSET, Integer.class);
+        
+        TYPE_MAP = Collections.unmodifiableMap(m);
+    }
+    
     public void process(Context context, Element dirElement) 
     throws IOException {
-        Element[] pElements = DomUtil.getChildElementsByTagName(dirElement, "p");
+        ClientProperties clientProperties = new ClientProperties();
+        try {
+            PropertyPeerFactory propertyPeerFactory = (PropertyPeerFactory) context.get(PropertyPeerFactory.class);
+            Element[] pElements = DomUtil.getChildElementsByTagName(dirElement, "p");
+            for (int i = 0; i < pElements.length; ++i) {
+                String propertyName = pElements[i].getAttribute("n");
+                Class propertyClass = (Class) TYPE_MAP.get(propertyName);
+                if (propertyClass == null) {
+                    throw new IOException("Illegal property in ClientProperties message: " + propertyName);
+                }
+                SerialPropertyPeer propertyPeer = propertyPeerFactory.getPeerForProperty(propertyClass);
+                Object propertyValue = propertyPeer.toProperty(context, propertyClass, pElements[i]);
+                clientProperties.setProperty(propertyName, propertyValue);
+            }
+        } catch (SerialException ex) {
+            throw new IOException("Cannot serialize ClientProperties: " + ex);
+        }
+        
+        UserInstance userInstance = (UserInstance) context.get(UserInstance.class);
+        userInstance.setClientProperties(clientProperties);
     }
 }
