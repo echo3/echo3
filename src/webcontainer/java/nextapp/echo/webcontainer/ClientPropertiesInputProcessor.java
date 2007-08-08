b9e7776c65d14f05a9c8433d8430d396a182db48
tliebeck <tliebeck@a486e859-b728-0410-9144-a388b9637126>
2007-08-08 03:25:53 +0000

    Docs/cleanups.

--- src/webcontainer/java/nextapp/echo/webcontainer/ClientPropertiesInputProcessor.java
+++ src/webcontainer/java/nextapp/echo/webcontainer/ClientPropertiesInputProcessor.java
@@ -1,3 +1,32 @@
+/* 
+ * This file is part of the Echo Web Application Framework (hereinafter "Echo").
+ * Copyright (C) 2002-2005 NextApp, Inc.
+ *
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ */
+
 package nextapp.echo.webcontainer;
 
 import java.io.IOException;
@@ -13,9 +42,19 @@ import nextapp.echo.app.serial.SerialPropertyPeer;
 import nextapp.echo.app.util.Context;
 import nextapp.echo.app.util.DomUtil;
 
+/**
+ * <code>ClientMessage.Processor</code> which generates a
+ * <code>ClientProperties</code> and assigns it to the
+ * relevant <code>UserInstance</code>.
+ */
 public class ClientPropertiesInputProcessor
 implements ClientMessage.Processor {
 
+    /**
+     * Mapping between property names and property classes,
+     * used for deserialization and to disallow setting
+     * of arbitrary properties.
+     */
     private static final Map TYPE_MAP;
     static {
         Map m = new HashMap();
@@ -35,6 +74,9 @@ implements ClientMessage.Processor {
         TYPE_MAP = Collections.unmodifiableMap(m);
     }
     
+    /**
+     * @see nextapp.echo.webcontainer.ClientMessage.Processor#process(nextapp.echo.app.util.Context, org.w3c.dom.Element)
+     */
     public void process(Context context, Element dirElement) 
     throws IOException {
         ClientProperties clientProperties = new ClientProperties();
