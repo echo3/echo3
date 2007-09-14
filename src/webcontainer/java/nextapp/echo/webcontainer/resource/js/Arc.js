f6fe13865f38e71f0c24009b3e0f1bcdfe4a1744
tliebeck <tliebeck@a486e859-b728-0410-9144-a388b9637126>
2007-09-14 02:23:08 +0000

    Arc API docs.

--- src/webcontainer/java/nextapp/echo/webcontainer/resource/js/Arc.js
+++ src/webcontainer/java/nextapp/echo/webcontainer/resource/js/Arc.js
@@ -3,12 +3,23 @@
  */
 EchoArc = function() { }
 
+/**
+ * @class Client for application-rendered components.  
+ * These clients are automatically created and destroyed by the
+ * ArcClient component synchronization peer.
+ */
 EchoArc.Client = function(application, domainElement) {
     EchoFreeClient.call(this, application, domainElement);
 };
 
 EchoArc.Client.prototype = EchoCore.derive(EchoFreeClient);
 
+/**
+ * @class Component synchronization peer for application rendered components.
+ * Application rendered component peers should extend this peer.
+ * The super-implementations of the renderAdd(), renderdispose(),
+ * renderDisplay(), and renderUpdate() methods must be invoked.
+ */
 EchoArc.ComponentSync = function() {
 
 };
@@ -16,14 +27,24 @@ EchoArc.ComponentSync = function() {
 EchoArc.ComponentSync.prototype = EchoCore.derive(EchoRender.ComponentSync);
 
 /**
+ * Creates the base component of that will be added to the root
+ * of the rendering application.  This component should probably be a
+ * ContentPane or other container.
+ * This method must be overridden by ARC implementations.
+ * 
  * @type EchoApp.Component
  */
 EchoArc.ComponentSync.prototype.createBaseComponent = function() { };
 
 /**
+ * Returns the element in which the client should be rendered.
+ * This method must be overridden by ARC implementations.
+ * 
  * @type Element
  */
 EchoArc.ComponentSync.prototype.getDomainElement = function() { };
+
+EchoRender.ComponentSync.prototype.renderAdd = function(update, parentElement) { };
 
 EchoArc.ComponentSync.prototype.renderDispose = function(update) {
     if (this.arcClient) {
@@ -48,3 +69,5 @@ EchoArc.ComponentSync.prototype.renderDisplay = function() {
         this.arcClient.init();
     }
 };
+
+EchoRender.ComponentSync.prototype.renderUpdate = function(update) { };
