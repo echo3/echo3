fe0bfbdca9c2e9ea156bbf16787e5150dde03196
tliebeck <tliebeck@a486e859-b728-0410-9144-a388b9637126>
2007-11-28 12:19:00 +0000

    Trivial refactorings to debug console.

--- src/webcontainer/java/nextapp/echo/webcontainer/resource/js/DebugConsole.js
+++ src/webcontainer/java/nextapp/echo/webcontainer/resource/js/DebugConsole.js
@@ -24,6 +24,9 @@ EchoDebugConsole = {
         EchoDebugConsole._windowElement.style.display = "none";
     },
     
+    /**
+     * Method which will overwrite Core.Debug.consoleWrite().
+     */
     _consoleWrite: function(text) {
         if (!EchoDebugConsole._initialized) {
             EchoDebugConsole._init();
@@ -38,59 +41,35 @@ EchoDebugConsole = {
     _init: function() {
         EchoDebugConsole._windowElement = document.createElement("div");
         EchoDebugConsole._windowElement.id = "__DebugConsole__";
-        EchoDebugConsole._windowElement.style.display = "none";
-        EchoDebugConsole._windowElement.style.position = "absolute";
-        EchoDebugConsole._windowElement.style.top = "20px";
-        EchoDebugConsole._windowElement.style.right = "20px";
-        EchoDebugConsole._windowElement.style.width = "300px";
-        EchoDebugConsole._windowElement.style.height = "300px";
-        EchoDebugConsole._windowElement.style.background = "#2f2f3f";
-        EchoDebugConsole._windowElement.style.color = "#3fff6f";
-        EchoDebugConsole._windowElement.style.border = "5px solid #3f6fff";
-        EchoDebugConsole._windowElement.style.overflow = "hidden";
-        EchoDebugConsole._windowElement.style.zIndex = 32767;
+        EchoDebugConsole._windowElement.style.cssText 
+                = "display:none;position:absolute;top:20px;right:20px;width:300px;height:300px;background-color:#2f2f3f;"
+                + "border:5px solid #3f6fff;overflow:hidden;z-index:32767;";
         
         var titleBarElement = document.createElement("div");
-        titleBarElement.style.position = "absolute";
-        titleBarElement.style.top = "1px";
-        titleBarElement.style.left = "1px";
-        titleBarElement.style.width = "278px";
-        titleBarElement.style.height = "20px";
-        titleBarElement.style.padding = "3px 10px";
-        titleBarElement.style.background = "#5f5f8f";
-        titleBarElement.style.color = "#ffffff";
-        titleBarElement.style.overflow = "hidden";
+        titleBarElement.style.cssText
+                = "position:absolute;top:1px;left:1px;width:278px;height:20px;padding:3px 10px;background-color:#5f5f8f;"
+                + "color:#ffffff;overflow:hidden;";
         titleBarElement.appendChild(document.createTextNode("/ Debug Console /"));
         EchoDebugConsole._windowElement.appendChild(titleBarElement);
     
         var clearButtonElement = document.createElement("span");
-        clearButtonElement.style.padding = "0px 0px 0px 20px";
-        clearButtonElement.style.cursor = "pointer";
+        clearButtonElement.style.cssText = "padding:0 0 0 20px;cursor:pointer;";
         clearButtonElement.appendChild(document.createTextNode("[clear]"));
         titleBarElement.appendChild(clearButtonElement);
         WebCore.DOM.addEventListener(clearButtonElement, "click", EchoDebugConsole._clearListener, false);
         
         var closeButtonElement = document.createElement("span");
-        closeButtonElement.style.padding = "0px 0px 0px 20px";
-        closeButtonElement.style.cursor = "pointer";
+        closeButtonElement.style.cssText = "padding:0 0 0 20px;cursor:pointer;";
         closeButtonElement.appendChild(document.createTextNode("[close]"));
         titleBarElement.appendChild(closeButtonElement);
         WebCore.DOM.addEventListener(closeButtonElement, "click", EchoDebugConsole._closeListener, false);
     
         EchoDebugConsole._contentElement = document.createElement("div");
-        EchoDebugConsole._contentElement.style.fontFamily = "monospace";
-        EchoDebugConsole._contentElement.style.fontSize = "9px";
-        EchoDebugConsole._contentElement.style.position = "absolute";
-        EchoDebugConsole._contentElement.style.top = "28px";
-        EchoDebugConsole._contentElement.style.left = "1px";
-        EchoDebugConsole._contentElement.style.width = "278px";
-        EchoDebugConsole._contentElement.style.height = "265px";
-        EchoDebugConsole._contentElement.style.padding = "3px 10px";
-        EchoDebugConsole._contentElement.style.background = "#1f1f2f";
-        EchoDebugConsole._contentElement.style.overflow = "auto";
+        EchoDebugConsole._contentElement.style.cssText = "font-family:monospace;font-size:9px;position:absolute;top:28px;left:1px;"
+                + "width:278px;height:265px;padding:3px 10px;background-color:#1f1f2f;overflow:auto;color:#3fff6f;";
         EchoDebugConsole._windowElement.appendChild(EchoDebugConsole._contentElement);
         
-        document.getElementsByTagName("body")[0].appendChild(EchoDebugConsole._windowElement);
+        document.body.appendChild(EchoDebugConsole._windowElement);
     
         EchoDebugConsole._initialized = true;
     },
