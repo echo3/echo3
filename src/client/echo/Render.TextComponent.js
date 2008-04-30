d3aef2ae1e35114d59eed6839e696996e8622990
tliebeck <tliebeck@a486e859-b728-0410-9144-a388b9637126>
2008-04-30 05:20:01 +0000

    Added +1px fudge factor to 100% text field renderings to avoid potential IE(7) horizontal scrollbar.

--- src/client/echo/Render.TextComponent.js
+++ src/client/echo/Render.TextComponent.js
@@ -77,7 +77,7 @@ Echo.Sync.TextComponent = Core.extend(Echo.Render.ComponentSync, {
         if (width == "100%" && this._input.parentNode.offsetWidth) {
             var border = this.component.render("border");
             var borderSize = Echo.Sync.Border.getPixelSize(this.component.render("border", "2px solid #000000"), "left")
-                    + Echo.Sync.Border.getPixelSize(this.component.render("border", "2px solid #000000"), "right");
+                    + Echo.Sync.Border.getPixelSize(this.component.render("border", "2px solid #000000"), "right") + 1;
             var adjustedPercent = 100 - Math.ceil(100 * borderSize / this._input.parentNode.offsetWidth);
             this._input.style.width = (adjustedPercent > 75 ? adjustedPercent : 75) + "%";
         }
