4129071f9b442d93055e5189d0e51bf1376baac9
tliebeck <tliebeck@a486e859-b728-0410-9144-a388b9637126>
2008-12-30 05:47:36 +0000

    TextComponent JS using measured scroll bar widths for IE6.

--- src/client/echo/Sync.TextComponent.js
+++ src/client/echo/Sync.TextComponent.js
@@ -147,9 +147,10 @@ Echo.Sync.TextComponent = Core.extend(Echo.Render.ComponentSync, {
             if (Core.Web.Env.BROWSER_INTERNET_EXPLORER) {
                 // Add default windows scroll bar width to border size for Internet Explorer browsers.
                 if (this._container) {
-                    this._container.style.width = this._adjustPercentWidth(100, 16, this._input.parentNode.offsetWidth) + "%";
+                    this._container.style.width = this._adjustPercentWidth(100, Core.Web.Measure.SCROLL_WIDTH, 
+                            this._input.parentNode.offsetWidth) + "%";
                 } else {
-                    borderSize += 16;
+                    borderSize += Core.Web.Measure.SCROLL_WIDTH;
                 }
             }
             this._input.style.width = this._adjustPercentWidth(parseInt(width, 10), borderSize, 
