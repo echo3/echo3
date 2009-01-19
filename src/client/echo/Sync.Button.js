49f637eef506a24503965c462c2a63bb20121bc6
tliebeck <tliebeck@a486e859-b728-0410-9144-a388b9637126>
2009-01-19 08:13:06 +0000

    Modified button rollover exit/button release code to return button to default state, even in case where client is locked.

--- src/client/echo/Sync.Button.js
+++ src/client/echo/Sync.Button.js
@@ -276,7 +276,7 @@ Echo.Sync.Button = Core.extend(Echo.Render.ComponentSync, {
     
     /** Processes a mouse button release event on the button, displaying the button's normal appearance. */
     _processRelease: function(e) {
-        if (!this.client || !this.client.verifyInput(this.component)) {
+        if (!this.client) {
             return true;
         }
         this.setPressedState(false);
@@ -293,7 +293,7 @@ Echo.Sync.Button = Core.extend(Echo.Render.ComponentSync, {
     
     /** Processes a mouse roll over exit event, displaying the button's normal appearance. */
     _processRolloverExit: function(e) {
-        if (!this.client || !this.client.verifyInput(this.component)) {
+        if (!this.client) {
             return true;
         }
         if (this._processRolloverExitRef) {
