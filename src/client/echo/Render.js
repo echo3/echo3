be081550e7e6dae6fae5d4911ee731b3f1194392
tliebeck <tliebeck@a486e859-b728-0410-9144-a388b9637126>
2009-02-16 09:39:15 +0000

    Improved error handling when renderFocus is not implemented.

--- src/client/echo/Render.js
+++ src/client/echo/Render.js
@@ -438,6 +438,10 @@ Echo.Render = {
     updateFocus: function(client) {
         var focusedComponent = client.application.getFocusedComponent();
         if (focusedComponent && focusedComponent.peer) {
+            if (!focusedComponent.peer.renderFocus) {
+                throw new Error("Cannot focus component: " + focusedComponent + 
+                        ", peer does not provide renderFocus() implemnetation."); 
+            }
             focusedComponent.peer.renderFocus();
         }
     }
