7657e2140839ec5d2c822e9580b0bca6ea2c1fbb
chrismay2 <mail@chrismay.de>
2013-05-03 14:35:54 +0200

    New feature Eclipse Support

--- src/server-java/testapp-interactive/eclipse/TestAppRun.java
+++ src/server-java/testapp-interactive/eclipse/TestAppRun.java
@@ -27,68 +27,45 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  */
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.ConnectException;
 import java.net.URL;
 import java.net.URLConnection;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import nextapp.echo.testapp.interactive.InteractiveServlet;
 
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.servlet.Context;
-import org.mortbay.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class TestAppRun {
-   
-    private static final Class SERVLET_CLASS = InteractiveServlet.class;
-    private static final String CONTEXT_PATH = "/TestApp";
-    private static final String PATH_SPEC = "/app";
-    private static final int PORT = 8001;
 
-    public static void main(String[] arguments)
-    throws Exception {
-        try {
-            URL url = new URL("http://localhost:" + PORT + "/__SHUTDOWN__/");
-            URLConnection conn = url.openConnection();
-            InputStream in = conn.getInputStream();
-            in.close();
-        } catch (ConnectException ex) {
-            // Do nothing.
-        }
-        
-        Server server = new Server(PORT);
-        
-        final Context testContext = new Context(server, CONTEXT_PATH, Context.SESSIONS);
-        testContext.addServlet(new ServletHolder(SERVLET_CLASS), PATH_SPEC);
-        
-        Context shutdownContext = new Context(server, "/__SHUTDOWN__");
-        shutdownContext.addServlet(new ServletHolder(new HttpServlet() {
+	private static final Class SERVLET_CLASS = InteractiveServlet.class;
+	private static final String CONTEXT_PATH = "/TestApp";
+	private static final String PATH_SPEC = "/app";
+	private static final int PORT = 8001;
 
-            private static final long serialVersionUID = 1L;
+	public static void main(String[] arguments) throws Exception {
 
-            protected void service(HttpServletRequest req, HttpServletResponse resp) 
-            throws ServletException, IOException {
-                try {
-                    // Manually stopping session handler to test Application.dispose().
-                    testContext.getSessionHandler().stop();
-                    System.out.println("Shutdown request received: terminating.");
-                    System.exit(0);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-            
-        }), "/");
+		try {
+			URL url = new URL("http://localhost:" + PORT + "/__SHUTDOWN__/");
+			URLConnection conn = url.openConnection();
+			InputStream in = conn.getInputStream();
+			in.close();
+		} catch (ConnectException ex) {
+			// Do nothing.
+		}
 
-        System.out.println("Deploying " + SERVLET_CLASS.getName() + " on http://localhost:" + PORT + CONTEXT_PATH + PATH_SPEC); 
-        
-        server.start();
-        server.join();
-    }
+		Server server = new Server(PORT);
+
+	    final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/app");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new InteractiveServlet()), "/");
+
+		System.out.println("Deploying " + SERVLET_CLASS.getName() + " on http://localhost:" + PORT + CONTEXT_PATH + PATH_SPEC);
+
+		server.start();
+		server.join();
+	}
 }
